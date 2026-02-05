601cc76a64ccb3ca679870954c93b01a1868dd51
steffiy <steffiy@gmail.com>
2026-02-05 02:35:38 +0000

    Update project files

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-// Add JS here
+const generateBtn = document.getElementById('generate-btn');
+const numberCircles = document.querySelectorAll('.number-circle');
+
+generateBtn.addEventListener('click', () => {
+    const generatedNumbers = new Set();
+    while (generatedNumbers.size < 6) {
+        generatedNumbers.add(Math.floor(Math.random() * 49) + 1);
+    }
+
+    numberCircles.forEach((circle, index) => {
+        circle.textContent = Array.from(generatedNumbers)[index];
+    });
+});
